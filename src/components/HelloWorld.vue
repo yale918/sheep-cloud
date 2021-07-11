cff10e86b96bfcd65d386242a089878685fe1331
yale918 <yale918@gmail.com>
2021-07-11 10:52:03 +0800

    3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,19 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      A very convenient cloud for you
+      <!-- <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>. -->
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Services</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://igclone918.herokuapp.com" target="_blank" rel="noopener">SheepCloud</a></li>
+      <li><a href="https://qforum918.herokuapp.com" target="_blank" rel="noopener">QForum</a></li>
+      <li><a href="https://yale918.github.io/asp/" target="_blank" rel="noopener">AppleSheep</a></li>
+      <li><a href="https://qforum918.herokuapp.com" target="_blank" rel="noopener">NFclone</a></li>
+      <li><a href="http://yale918.byethost7.com/" target="_blank" rel="noopener">Discuz</a></li>
+      <li><a href="https://fathomless-stream-65606.herokuapp.com/" target="_blank" rel="noopener">ioChat</a></li>
     </ul>
+    <!--
     <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
@@ -27,6 +31,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    -->
   </div>
 </template>
 
